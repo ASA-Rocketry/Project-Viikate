414824343061437392ffcbe6b0efaf12ae4a6a5e
Artur Roos <artur.roos@ktnlvr.dev>
2026-05-26 23:22:16 +0300

    Wipe everything

--- Guidance, Navigation and Control/Flight Code/include/flight_data.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLIGHT_CODE_INCLUDE_FLIGHT_DATA_H_
-#define FLIGHT_CODE_INCLUDE_FLIGHT_DATA_H_
-
-struct FlightData;
-
-FlightData getSimulatedFlightData();
-void readSimulatedData();
-
-#endif  // FLIGHT_CODE_INCLUDE_FLIGHT_DATA_H_
