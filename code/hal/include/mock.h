04fc188dd7bfafdbd4a3bbd4b194dd49c31df4ea
Jozef Jabczun <jabczunjozef@gmail.com>
2026-05-27 21:21:49 +0300

    style: adjust .clang-format from Google style to Rust-like style

--- code/hal/include/mock.h
+++ code/hal/include/mock.h
@@ -9,10 +9,10 @@
 #define MAX_MOCK_HAL_PIN_COUNT 64
 
 typedef struct Hal {
-  // DDR(Artur): Canary, is set to the pointer the HAL itself after
-  // initialization. This is needed to avoid accidential reinitializations or
-  // movements of the HAL.
-  Hal* canary;
-  pin_mode pin_modes[MAX_MOCK_HAL_PIN_COUNT];
-  bool pin_values[MAX_MOCK_HAL_PIN_COUNT];
+    // DDR(Artur): Canary, is set to the pointer the HAL itself after
+    // initialization. This is needed to avoid accidential reinitializations or
+    // movements of the HAL.
+    Hal *canary;
+    pin_mode pin_modes[MAX_MOCK_HAL_PIN_COUNT];
+    bool pin_values[MAX_MOCK_HAL_PIN_COUNT];
 } Hal;
