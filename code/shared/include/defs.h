12db4f4e8a240ac4a237fd3abcd3825b3f7d31a6
Jozef Jabczun <jabczunjozef@gmail.com>
2026-05-28 16:20:37 +0300

    feat: Add more typedefs for primitive types

--- code/shared/include/defs.h
+++ code/shared/include/defs.h
@@ -1,13 +1,34 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #pragma once
 
 typedef unsigned char bool;
+
 typedef int i32;
+typedef short i16;
+typedef char i8;
+
 typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
 typedef float f32;
 typedef double f64;
+
+static_assert(sizeof(bool) == 1, "bool must be 1 byte");
+
+static_assert(sizeof(i32) == 4, "i32 must be 4 bytes");
+static_assert(sizeof(i16) == 2, "i16 must be 2 bytes");
+static_assert(sizeof(i8) == 1, "i8 must be 1 byte");
+
+static_assert(sizeof(u32) == 4, "u32 must be 4 bytes");
+static_assert(sizeof(u16) == 2, "u16 must be 2 bytes");
+static_assert(sizeof(u8) == 1, "u8 must be 1 byte");
+
+static_assert(sizeof(f32) == 4, "f32 must be 4 bytes");
+static_assert(sizeof(f64) == 8, "f64 must be 8 bytes");
 
 #define true ((bool)1)
 #define false ((bool)0)
